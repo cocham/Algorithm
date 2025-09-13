dce84f94ef38e8e39b53149e7ae0fb5cf3b4867c
cocham <91892334+cocham@users.noreply.github.com>
2025-09-13 23:41:49 +0900

    [Bronze III] Title: 제곱 수?, Time: 112 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Bronze/34027. 제곱 수？/제곱 수？.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testN = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < testN; i++) {
+            int n = Integer.parseInt(br.readLine());
+            long r = (long) Math.sqrt(n);
+            if (r * r == n) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+        
+        System.out.print(sb);
+    }
+}
