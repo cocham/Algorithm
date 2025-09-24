d2a0a2b93870b34802a2252d61d9fa295ccc2c2d
cocham <91892334+cocham@users.noreply.github.com>
2025-09-24 19:41:00 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static boolean[] used;
+    static int[] pick;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        pick = new int[m];
+        
+        dfs(0, 1);
+        
+        System.out.print(sb); 
+    }
+    
+    static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(pick[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        
+        for (int x = start; x <= n; x++) {
+            pick[depth] = x;
+            dfs(depth + 1, x + 1);
+        }
+    }
+    
+}
