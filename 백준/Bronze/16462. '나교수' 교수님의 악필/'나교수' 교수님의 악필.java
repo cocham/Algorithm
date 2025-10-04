7b0b695f12f623aad830b30630e50a7e0fabb16b
cocham <91892334+cocham@users.noreply.github.com>
2025-10-04 23:16:58 +0900

    [Bronze II] Title: '나교수' 교수님의 악필, Time: 144 ms, Memory: 16200 KB -BaekjoonHub

---
+++ 백준/Bronze/16462. '나교수' 교수님의 악필/'나교수' 교수님의 악필.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long n = Long.parseLong(br.readLine());
+        int average = 0;
+        
+        long allScore = 0;
+        for (int i = 0; i < n; i++) {
+            String score = br.readLine();
+            if (score.equals("100")) {
+                allScore += 100;
+            } else {
+                score = score.replaceAll("[069]", "9");
+                allScore += Long.parseLong(score);
+            }
+        }
+        
+        System.out.println(Math.round((double) allScore / n));
+    }
+}
