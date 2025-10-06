85a6e512abf226863e081c4e2b3ef28ec16cabcd
cocham <91892334+cocham@users.noreply.github.com>
2025-10-06 23:50:40 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 312 ms, Memory: 30880 KB -BaekjoonHub

--- 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.java
+++ 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.java
@@ -10,36 +10,33 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String[] skills = br.readLine().split("");
         
-        List<String> numSkills = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
         int lCnt = 0;
         int sCnt = 0;
         int cnt = 0;
         for (String s : skills) {
-            if (numSkills.contains(s)) {
-                cnt += 1;
-            } else {
-                if (s.equals("L")) {
-                    lCnt++;
-                } else if (s.equals("S")) {
-                    sCnt++;
-                } else if (s.equals("R")) {
-                    if (lCnt > 0) {
-                        lCnt--;
-                        cnt++;
-                    } else {
-                        break;
-                    }
-                } else if (s.equals("K")) {
-                    if (sCnt > 0) {
-                        sCnt--;
-                        cnt++;
-                    } else {
-                        break;
-                    }
+            if (s.equals("L")) {
+                lCnt++;
+            } else if (s.equals("S")) {
+                sCnt++;
+            } else if (s.equals("R")) {
+                if (lCnt > 0) {
+                    lCnt--;
+                    cnt++;
+                } else {
+                    break;
                 }
+            } else if (s.equals("K")) {
+                if (sCnt > 0) {
+                    sCnt--;
+                    cnt++;
+                } else {
+                    break;
+                }
+            } else {
+                cnt++;
             }
         }
-        
+
         System.out.print(cnt);
     }
 }
