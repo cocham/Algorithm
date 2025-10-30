8ca1e7d1121d27e4e3ab945d46588da19f2583a8
cocham <91892334+cocham@users.noreply.github.com>
2025-10-31 00:38:33 +0900

    [Silver III] Title: 파일 구별하기, Time: 156 ms, Memory: 17900 KB -BaekjoonHub

--- 백준/Silver/2371. 파일 구별하기/파일 구별하기.java
+++ 백준/Silver/2371. 파일 구별하기/파일 구별하기.java
@@ -4,51 +4,51 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         
-        int n = sc.nextInt();
-        List<int[]> files = new ArrayList<>(n);
-        List<Integer> cur = new ArrayList<>();
+        List<List<Integer>> files = new ArrayList<>(n);
         
-        while(files.size() < n) {
-            int v = sc.nextInt();
-            if (v == -1) {
-                int[] arr = cur.stream()
-                               .mapToInt(Integer::intValue)
-                               .toArray();
-                files.add(arr);
-                cur.clear();
-            } else {
-                cur.add(v);
-            }
-        }
-        
-        StringBuilder[] sig = new StringBuilder[n];
         for (int i = 0; i < n; i++) {
-            sig[i] = new StringBuilder();
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            List<Integer> file = new ArrayList<>();
+
+            while(st.hasMoreTokens()) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num != -1) {
+                    file.add(num);
+                } else {
+                    break;
+                }
+            }
+            files.add(file);
         }
         
+        StringBuilder[] cut = new StringBuilder[n];
+        for (int i = 0; i < n; i++) {
+            cut[i] = new StringBuilder();
+        }
         for (int k = 0; ; k++) {
             Set<String> seen = new HashSet<>();
             for (int i = 0; i < n; i++) {
-                if (k < files.get(i).length) {
-                    sig[i].append(files.get(i)[k]).append(',');
+                if (k < files.get(i).size()) {
+                    int p = files.get(i).get(k);
+                    cut[i].append(p).append(',');
                 } else {
-                    sig[i].append(0).append(',');
+                    cut[i].append(0).append(',');
                 }
-                seen.add(sig[i].toString());
+                seen.add(cut[i].toString());
             }
             if (seen.size() == n) {
                 System.out.println(k + 1);
                 break;
             }
         }
-            
-    } 
+    }
 }
