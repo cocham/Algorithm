96974051a6368f9db765fd1dfee03d3a6d821ebe
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-01-07 16:47:49 +0900

    [Silver II] Title: 과일 탕후루, Time: 276 ms, Memory: 29624 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] fruits = new int[n];
+        
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());        
+        for (int i = 0; i < n; i++) {
+            fruits[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int left = 0, kind = 0, maxLen = 0;
+        int[] counts = new int[10];
+        
+        for (int right = 0; right < n; right++) {
+            
+            if (counts[fruits[right]] == 0) {
+                kind++;
+            }
+            
+            counts[fruits[right]]++;
+            
+            while (kind > 2) {
+                counts[fruits[left]]--;
+                if (counts[fruits[left]] == 0) {
+                    kind --;
+                }
+                
+                left++;
+            }
+            
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+       
+        
+        System.out.print(maxLen);
+    }
+}
