fabe2d8cf7046636fd449db28195403d9aaa5b03
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-01-08 15:14:02 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] house;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        house = new int[n][3];
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            house[i][0] = r;
+            house[i][1] = g;
+            house[i][2] = b;
+        }
+        
+        int[][] dp = new int[n][3];
+        dp[0][0] = house[0][0];
+        dp[0][1] = house[0][1];
+        dp[0][2] = house[0][2];        
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + house[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + house[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + house[i][2];
+        }
+        
+        System.out.println(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
+    }
+}
