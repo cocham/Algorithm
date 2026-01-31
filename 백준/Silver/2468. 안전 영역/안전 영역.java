3e0a923529ce1b5381cf82b39abc08a11f16ce5f
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-01-31 14:03:20 +0900

    [Silver I] Title: 안전 영역, Time: 256 ms, Memory: 55280 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,87 +1,80 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Arrays;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class Main {
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
     static int N;
-    static int[][] map;
-    static int[] dx = {-1, 1, 0, 0}; //상하좌우
-    static int[] dy = {0, 0, -1, 1};
-    static boolean[][] check;
-     
-    static class Location {
+    static int[][] arr;
+    static boolean[][] visited;
+       
+    static class Position {
         int x;
         int y;
         
-        Location(int x, int y) {
+        Position(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
     
-    static void resetCheckArray() {
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(check[i], false);
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        check = new boolean[N][N];
-        
         StringTokenizer st;
+        
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
-        int cnt = 1;
-        for (int h = 1; h <= 100; h++) {
-            resetCheckArray();
+        int cnt = 0;
+        for (int h = 0; h <= 100; h++) {
+            visited = new boolean[N][N];
             int curCnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    int H = map[i][j];
-                    if (H > h && !check[i][j]) {
-                        bfs(h, new Location(i, j));
+                    if (arr[i][j] > h && !visited[i][j]) {
+                        visited[i][j] = true;
+                        bfs(i,j,h);
                         curCnt++;
                     }
                 }
             }
+            
             cnt = Math.max(curCnt, cnt);
         }
         
-        System.out.println(cnt);
+        System.out.print(cnt);
     }
     
-    static void bfs(int standardH, Location location) {
-        Deque<Location> adj = new ArrayDeque<>();
-        adj.addLast(location);
+    static void bfs(int x, int y, int h) {
+        Position pos = new Position(x, y);
+        Queue<Position> q = new LinkedList<>();
+        q.add(pos);
         
-        while (!adj.isEmpty()) {
-            Location curLocation = adj.poll();
+        while(!q.isEmpty()) {
+            Position curPos = q.poll();
             
             for (int i = 0; i < 4; i++) {
-                int moveX = curLocation.x + dx[i];
-                int moveY = curLocation.y + dy[i];
+                int moveX = curPos.x + dx[i];
+                int moveY = curPos.y + dy[i];
                 
                 if (moveX >= 0 && moveX < N && moveY >= 0 && moveY < N) {
-                     if (map[moveX][moveY] > standardH && !check[moveX][moveY]) {
-                         adj.add(new Location(moveX, moveY));
-                         check[moveX][moveY] = true;
-                     }   
+                    if (arr[moveX][moveY] > h && !visited[moveX][moveY]) {
+                        q.add(new Position(moveX, moveY));
+                        visited[moveX][moveY] = true;
+                        
+                    }
                 }
             }
-            
         }
     }
-       
 }
