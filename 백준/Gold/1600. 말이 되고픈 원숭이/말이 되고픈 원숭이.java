12395624f581d0985bbdd0d1720592e7ee62e4b7
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-02-13 13:23:23 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 444 ms, Memory: 89496 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,32 +1,30 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
-    static int[] dr = {-2, -1, -2, -1, 1, 2, 2, 1};
-    static int[] dc = {1, 2, -1, -2, -2, -1, 1, 2};
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int W;
-    static int H;
+    static int[] dr = {-2, -1, 1, 2, 2, 1, -1, -2};
+    static int[] dc = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int[] mdr = {-1,1,0,0};
+    static int[] mdc = {0,0,-1,1};
+    static int K, W, H;
     static int[][] arr;
     static boolean[][][] visited;
-    static int K;
     
     static class Position {
-        int x;
-        int y;
+        int r;
+        int c;
         int step;
-        int skill;
+        int kCnt;
         
-        Position(int x, int y, int step, int skill) {
-            this.x = x;
-            this.y = y;
+        Position(int r, int c, int step, int kCnt) {
+            this.r = r;
+            this.c = c;
             this.step = step;
-            this.skill = skill;
+            this.kCnt = kCnt;
         }
     }
     
@@ -36,22 +34,19 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         W = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
-        arr = new int[H][W];
-        visited = new boolean[H][W][K+1];
         
-        for (int h = 0; h < H; h++) {
+        arr = new int[H][W];
+        visited = new boolean[H][W][K + 1];
+        for (int i = 0; i < H; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int w = 0; w < W; w++) {
-                arr[h][w] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < W; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
         int cnt = bfs();
-        if (cnt < 0) {
-            System.out.print(-1);
-        } else {
-            System.out.print(cnt);
-        }
+        System.out.print(cnt);
+
     }
     
     static int bfs() {
@@ -59,39 +54,42 @@ public class Main {
         q.add(new Position(0,0,0,0));
         visited[0][0][0] = true;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Position cur = q.poll();
-            if (cur.x == H - 1 && cur.y == W - 1) {
+            
+            if (cur.r == H - 1 && cur.c == W - 1) {
                 return cur.step;
             }
-               
-            if (cur.skill < K) {
+            
+            if (cur.kCnt < K) {
                 for (int i = 0; i < 8; i++) {
-                    int nr = cur.x + dr[i];
-                    int nc = cur.y + dc[i];
-                    int nextSkill = cur.skill + 1;
+                    int nr = cur.r + dr[i];
+                    int nc = cur.c + dc[i];
+                    int kCnt = cur.kCnt + 1;
                     
                     if (nr >= 0 && nr < H && nc >= 0 && nc < W) {
-                        if (!visited[nr][nc][nextSkill] && arr[nr][nc] == 0) {
-                            q.add(new Position(nr, nc, cur.step + 1, nextSkill));
-                            visited[nr][nc][nextSkill] = true;
-                        }    
+                        if (!visited[nr][nc][kCnt] && arr[nr][nc] == 0) {
+                            visited[nr][nc][kCnt] = true;
+                            q.add(new Position(nr, nc, cur.step + 1, kCnt));
+                        }
                     }
                 }
             }
             
             for (int i = 0; i < 4; i++) {
-                int nr = cur.x + dx[i];
-                int nc = cur.y + dy[i];
+                int nr = cur.r + mdr[i];
+                int nc = cur.c + mdc[i];
                 
                 if (nr >= 0 && nr < H && nc >= 0 && nc < W) {
-                    if (!visited[nr][nc][cur.skill] && arr[nr][nc] == 0) {
-                        q.add(new Position(nr, nc, cur.step + 1, cur.skill));
-                        visited[nr][nc][cur.skill] = true;
-                    }    
+                    if (!visited[nr][nc][cur.kCnt] && arr[nr][nc] == 0) {
+                        visited[nr][nc][cur.kCnt] = true;
+                        q.add(new Position(nr, nc, cur.step + 1, cur.kCnt));
+                    }
                 }
             }
+
         }
+
         return -1;
     }
 }
