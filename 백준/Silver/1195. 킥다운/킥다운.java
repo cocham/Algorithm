e43f914ca6078a8ca0b1fdb2b6b85114ec73bf8a
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-02-14 21:26:40 +0900

    [Silver I] Title: 킥다운, Time: 88 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1195. 킥다운/킥다운.java
+++ 백준/Silver/1195. 킥다운/킥다운.java
@@ -1,44 +1,43 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String fst = br.readLine();
-        String sec = br.readLine();
-        int minLength = fst.length() + sec.length();
+        String g1 = br.readLine();
+        String g2 = br.readLine();
         
-        for (int i = 0; i < fst.length(); i++) {
-            if(canFit(fst, sec, i)) {
-                int width = Math.max(fst.length(), i + sec.length());
-                minLength = Math.min(minLength, width);
-            }
-        }
+        int len1 = solve(g1, g2);
+        int len2 = solve(g2, g1);
         
-        for (int i = 0 ; i < sec.length(); i++) {
-            if(canFit(sec, fst, i)) {
-                int width = Math.max(sec.length(), i + fst.length());
-                minLength = Math.min(minLength, width);
-            }
-        }
-        
-        System.out.println(minLength);
-    } 
+        System.out.print(Math.min(len1, len2));
+    }
     
-    static boolean canFit(String base, String move, int offset) {
-        for (int j = 0; j < move.length(); j++) {
-            int baseIdx = offset + j;
-            
-            if (baseIdx >= base.length()) {
-                break;
+    static int solve(String base, String move) {
+        int minLen = base.length() + move.length();
+        
+        for (int i = 0; i < base.length(); i++) {
+            if (check(base, move, i)) {
+                 int len = Math.max(base.length(), move.length() + i);  
+                 minLen = Math.min(len, minLen);
             }
+        }
+        
+        return minLen;
+    }
+    
+    static boolean check(String base, String move, int dist) {
+        for (int j = 0; j < move.length(); j++) {
+            int baseIdx = j + dist;
+            
+            if (baseIdx >= base.length()) break;
             
             if (base.charAt(baseIdx) == '2' && move.charAt(j) == '2') {
                 return false;
             }
         }
-        
         return true;
     }
 }
