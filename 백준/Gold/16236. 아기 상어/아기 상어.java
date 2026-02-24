a331f4f2477a1cec8ba4e8a6f653fb709126b8cf
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-02-24 17:02:50 +0900

    [Gold III] Title: 아기 상어, Time: 168 ms, Memory: 24676 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,36 +1,42 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
     static int N;
     static int[][] arr;
-    static int[] dr = {1,-1,0,0};
-    static int[] dc = {0,0,-1,1};
     static int sharkR, sharkC;
-    static int sSize = 2;
-    static int eaten = 0;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
     
-    static class Position {
-        int r, c;
+    static class Shark {
+        int size, ateCnt; 
         
-        Position(int r, int c) {
-            this.r = r;
-            this.c = c;
+        Shark(int size, int ateCnt) {
+            this.size = size;
+            this.ateCnt = ateCnt;
         }
     }
     
-    static class Fish {
+    static class Fish implements Comparable<Fish> {
         int r, c, dist;
         
         Fish (int r, int c, int dist) {
             this.r = r;
             this.c = c;
             this.dist = dist;
+        }
+        
+        @Override 
+        public int compareTo(Fish o) {
+            if (this.dist != o.dist) {
+                return this.dist - o.dist;
+            }
+            if (this.r != o.r) {
+                return this.r - o.r;
+            }
+            return this.c - o.c;
         }
     }
     
@@ -51,75 +57,65 @@ public class Main {
             }
         }
         
+        int sharkSize = 2;
+        int eatCnt = 0;
         int totalTime = 0;
+        
         while (true) {
-            Fish target = bfs();
+            Fish target = findFish(sharkR, sharkC, sharkSize);
             
             if (target == null) break;
             
             totalTime += target.dist;
             arr[target.r][target.c] = 0;
-            
             sharkR = target.r;
             sharkC = target.c;
             
-            eaten++;
-            if (eaten == sSize) {
-                sSize ++;
-                eaten = 0;
+            eatCnt++;
+
+            if (eatCnt == sharkSize) {
+                sharkSize++;
+                eatCnt = 0;
             }
         }
         
-        System.out.print(totalTime);
+        System.out.print(totalTime);        
     }
     
-    static Fish bfs() {
-        Queue<Position> q = new LinkedList<>();
-        int[][] times = new int[N][N];
-        
+    static Fish findFish(int row, int col, int s) {
+        int[][] dist = new int[N][N];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                times[i][j] = -1;
-            }
+            Arrays.fill(dist[i], -1);
         }
-        
-        q.add(new Position(sharkR, sharkC));
-        times[sharkR][sharkC] = 0;
-        PriorityQueue<Fish> candidates = new PriorityQueue<>((o1, o2) -> {
-            if (o1.dist == o2.dist) {
-                if (o1.r == o2.r) {
-                    return o1.c - o2.c;
-                }
-                return o1.r - o2.r;
-            }
-            return o1.dist - o2.dist;
-        });
-        
+        dist[row][col] = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{row,col});
+        ArrayList<Fish> candidates = new ArrayList<>();
+    
         while (!q.isEmpty()) {
-            Position cur = q.poll();
-            int r = cur.r;
-            int c = cur.c;
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
             
             for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                
+            
                 if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
-                if (arr[nr][nc] > sSize) continue;
+                if (dist[nr][nc] != -1 || arr[nr][nc] > s) continue;
                 
-                if (times[nr][nc] == -1) {
-                    times[nr][nc] = times[r][c] + 1;
-                    q.add(new Position(nr, nc));
-                    
-                    if (arr[nr][nc] != 0 && arr[nr][nc] < sSize) {
-                        candidates.add(new Fish(nr, nc, times[nr][nc]));
-                    }
+                dist[nr][nc] = dist[r][c] + 1;
+                q.add(new int[] {nr,nc});
+
+                if (arr[nr][nc] != 0 && arr[nr][nc] < s) {
+                    candidates.add(new Fish(nr, nc, dist[nr][nc]));
                 }
             }
         }
-        
+
         if (candidates.isEmpty()) return null;
-        
-        return candidates.poll();
+
+        Collections.sort(candidates);
+        return candidates.get(0);
     }
 }
