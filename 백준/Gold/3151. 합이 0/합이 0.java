814a59f54669e35023d4b48593b7583ab6f0720c
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-02-27 15:39:01 +0900

    [Gold IV] Title: 합이 0, Time: 392 ms, Memory: 18940 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -5,59 +5,59 @@ import java.util.StringTokenizer;
 import java.util.Arrays;
 
 public class Main {
-    static int[] arr;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         
-        long ans = 0;
-        for (int i = 0; i < N - 2; i++) {
+        long team = 0;
+        for (int i = 0; i < n; i++) {
+            int stand = arr[i];
+            
             int left = i + 1;
-            int right = N - 1;
+            int right = n - 1;
             
             while (left < right) {
-                long sum = (long) arr[i] + arr[left] + arr[right];
+                int sum = stand + arr[left] + arr[right];
                 
                 if (sum == 0) {
-                    if (arr[left] != arr[right]) {
-                        int leftCount = 0;
-                        int rightCount = 0;
-                        
-                        int leftValue = arr[left];
-                        while (left < right && arr[left] == leftValue) {
-                            leftCount++;
-                            left++;
-                        }
-                        
-                        int rightValue = arr[right];
-                        while (right >= left && arr[right] == rightValue) {
-                            rightCount++;
-                            right--;
-                        }
-                        
-                        ans += leftCount * rightCount;
-                    } else if (arr[left] == arr[right]) {
-                        int sameCnt = right - left + 1;
-                        ans += (long) sameCnt * (sameCnt - 1) / 2;
+                    if (arr[left] == arr[right]) {
+                        long sameCnt = right - left + 1;
+                        team += sameCnt * (sameCnt - 1) / 2;
                         break;
                     }
-                } else if (sum < 0) {
+                    
+                    int l = 1;
+                    int r = 1;
+                   
+                    while (arr[left] == arr[left + 1]) {
+                        left++;
+                        l++;
+                    }
+                    
+                    while (arr[right] == arr[right - 1]) {
+                        right--;
+                        r++;
+                    }
+                    
+                    team += l * r;
                     left++;
-                } else if (sum > 0) {
                     right--;
                 }
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                }
             }
-
         }
         
-        System.out.print(ans);
+        System.out.print(team);
     }
 }
