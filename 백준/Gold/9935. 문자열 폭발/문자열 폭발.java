19154a8c4553c823e421eadf0dae694fce1d77e5
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-03-03 14:13:56 +0900

    [Gold IV] Title: 문자열 폭발, Time: 460 ms, Memory: 62148 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,56 +1,51 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.StringTokenizer;
 import java.util.Stack;
 
 public class Main {
-    static String str;
+    static Stack<Character> stack = new Stack<>();
     static String bomb;
-    static Stack<Character> chars;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        str = br.readLine();
+        String line = br.readLine();
         bomb = br.readLine();
-        chars = new Stack<>();
-        
-        char target = bomb.charAt(bomb.length() - 1);
-        for (int i = 0; i < str.length(); i++) {
-            chars.push(str.charAt(i));
-            if (chars.peek() == target) {
-                check();
+        int bombLen = bomb.length();
+
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            stack.push(c);
+
+            if (c == bomb.charAt(bombLen - 1)) {
+                if (stack.size() < bombLen) continue;
+                
+                boolean isBomb = true;
+
+                for (int j = 0; j < bombLen; j++) {
+                    if (stack.get(stack.size() - bombLen + j) != bomb.charAt(j)) {
+                        isBomb = false;
+                        break;
+                    }
+                }
+                
+                if (isBomb) {
+                    for (int j = 0; j < bombLen; j++) {
+                        stack.pop();
+                    }
+                }
             }
         }
         
-        while (!chars.isEmpty()) {
-            sb.append(chars.pop());
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
-        
         if (sb.length() == 0) {
             System.out.print("FRULA");
         } else {
             System.out.print(sb.reverse().toString());
         }
-    }
-    
-    static void check() {
-        if (chars.size() < bomb.length()) return;
-        
-        boolean isMatch = true;
-        
-        for (int i = 0; i < bomb.length(); i++) {
-            if (chars.get(chars.size() - bomb.length() + i) != bomb.charAt(i)) {
-                isMatch = false;
-                break;
-            }
-        }
-
-        if (isMatch) {
-            for (int i = 0; i < bomb.length(); i++) {
-                chars.pop();
-            }
-        }
-        
     }
 }
