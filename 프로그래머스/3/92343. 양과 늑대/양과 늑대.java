5fb6927db15edc96b2fc1e02cf9537c8af43fab6
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-03-03 17:02:28 +0900

    [level 3] Title: 양과 늑대, Time: 7.42 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
-import java.util.List;
 
 class Solution {
     static ArrayList<Integer>[] tree;
-    static int maxSheep = 0;
+    static int maxSheep;
     static int[] info;
     
     public int solution(int[] info, int[][] edges) {
+        
         this.info = info;
+        
         tree = new ArrayList[info.length];
         for (int i = 0; i < info.length; i++) {
             tree[i] = new ArrayList<>();
@@ -19,39 +20,31 @@ class Solution {
             tree[root].add(child);
         }
         
-        dfs(0,1,0,tree[0]);
+        dfs(0, 1, 0, tree[0]);
         return maxSheep;
     }
     
-    void dfs(int cur, int sheep, int wolf, List<Integer> nextNodes) {
-        
+    static void dfs(int node, int sheep, int wolf, ArrayList<Integer> nextNodes) {
         if (wolf >= sheep) {
             return;
         }
         
-        maxSheep = Math.max(maxSheep, sheep);
+        maxSheep = Math.max(sheep, maxSheep);
         
         for (int i = 0; i < nextNodes.size(); i++) {
-            int next = nextNodes.get(i);
-
-            List<Integer> copy = new ArrayList<>();
-            for (int j = 0; j < nextNodes.size(); j++) {
-                if (i == j) continue;
-                copy.add(nextNodes.get(j));
-            }
+            int next = nextNodes.get(i); //child
             
-            for (int child: tree[next]) {
-                copy.add(child);
-            }
+            ArrayList<Integer> nextNext = new ArrayList<>(nextNodes);
             
-            if (info[next] == 0) {
-                dfs(next, sheep + 1, wolf, copy);
-            } else if (info[next] == 1) {
-                dfs(next, sheep, wolf + 1, copy);
-            }
+            nextNext.remove(Integer.valueOf(next));
             
+            nextNext.addAll(tree[next]);
+            
+            if (info[next] == 1) {
+                dfs(next, sheep, wolf + 1, nextNext);
+            } else if (info[next] == 0) {
+                dfs(next, sheep + 1, wolf, nextNext);
+            }
         }
-        
-        
     }
 }
