4565e8ff035388a0b22fc5339a725d2337538118
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-03-04 17:43:07 +0900

    [level 3] Title: 입국심사, Time: 100.05 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        
+        long left = 1;
+        long right = (long) times[times.length - 1] * n;
+        long answer = right;
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            
+            long sum = 0;
+            
+            for (int time : times) {
+                sum += mid / time;
+            }
+            
+            if (sum >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            
+        }
+
+        return answer;
+    }
+}
