0dcdbf7f1a099d08a8e3134a7a6afd2573df2f12
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-03-06 09:58:19 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 54.11 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,53 +1,56 @@
 class Solution {
-    static int maxCnt;
-    static int maxSold;
-    static int[] discountRates = {10, 20, 30, 40};
+    static int[] discount = {10,20,30,40};
+    static int[] dc;
+    static int userCnt;
+    static int totalPay;
     
     public int[] solution(int[][] users, int[] emoticons) {
-        int[] discounts = new int[emoticons.length];
+        dc = new int[emoticons.length];
         
-        dfs(0, discounts, users, emoticons);
-        int[] answer = {maxCnt, maxSold};
+        dfs(0,users,emoticons);
+        int[] answer = {userCnt, totalPay};
         return answer;
     }
     
-    static void dfs(int depth, int[] discounts, int[][] users, int[] emoticons) {
-        if (depth == emoticons.length) {
-            int curCnt = 0;
-            int curSold = 0;
+    static void dfs(int emoIdx, int[][] users, int[] emoticons) {
+        if (emoIdx == emoticons.length) {
+            int curUserCnt = 0;
+            int curPay = 0;
             
             for (int[] user : users) {
-                int userDis = user[0];
+                int userDc = user[0];
                 int userPay = user[1];
-                int userSum = 0;
+                int thisPay = 0;
                 
-                for (int i = 0; i < discounts.length; i++) {
-                    if (discounts[i] >= userDis) {
-                        userSum += emoticons[i] - (emoticons[i] * discounts[i] / 100);
+                for (int i = 0; i < emoticons.length; i++) {
+                    int emoDc = dc[i];
+                    if (emoDc >= userDc) {
+                        int emoCost = emoticons[i] - (emoticons[i] * dc[i] / 100);
+                        thisPay += emoCost;
                     }
                 }
                 
-                if (userSum >= userPay) {
-                    curCnt++;
+                if (thisPay >= userPay) {
+                    curUserCnt++;
                 } else {
-                    curSold += userSum;
+                    curPay += thisPay;
                 }
             }
             
-            if (curCnt > maxCnt) {
-                maxCnt = curCnt;
-                maxSold = curSold;
-            } else if (curCnt == maxCnt && curSold > maxSold) {
-                maxSold = curSold;
+            if (curUserCnt > userCnt) {
+                userCnt = curUserCnt;
+                totalPay = curPay;
+            } if (userCnt == curUserCnt && curPay > totalPay) {
+                totalPay = curPay;
             }
             
             return;
         }
         
-        for (int i = 0; i < 4; i++) {
-            discounts[depth] = discountRates[i];
-            dfs(depth + 1, discounts, users, emoticons);
-        }
         
+        for (int i = 0; i < 4; i++) {
+            dc[emoIdx] = discount[i];
+            dfs(emoIdx + 1, users, emoticons);
+        }
     }
 }
