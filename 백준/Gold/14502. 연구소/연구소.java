9859c0e96b9538d97e2637a3fc71f2351772b01e
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-03-26 13:47:54 +0900

    [Gold IV] Title: 연구소, Time: 444 ms, Memory: 147252 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,125 +1,106 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
-    static int N;
-    static int M;
+    static int N, M;
     static int[][] arr;
-    static int[][] tempArr;
-    static boolean[][] visited;
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
-    
-    static class Position {
-        int x;
-        int y;
-        
-        Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static boolean[] visited;
+    static ArrayList<int[]> walls = new ArrayList<>();
+    static int maxSafe = 0;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
-        visited = new boolean[N][M];
         
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        List<Position> blanks = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+                
                 if (arr[i][j] == 0) {
-                    blanks.add(new Position(i, j));
+                    walls.add(new int[]{i,j});
                 }
             }
         }
         
-        int maxCnt = 0;
-        for (int i = 0; i < blanks.size(); i++) {
-            for (int j = i + 1; j < blanks.size(); j++) {
-                for (int k = j + 1; k < blanks.size(); k++) {
-                    Position wall1 = blanks.get(i);
-                    Position wall2 = blanks.get(j);
-                    Position wall3 = blanks.get(k);
-                    
-                    arr[wall1.x][wall1.y] = 1;
-                    arr[wall2.x][wall2.y] = 1;
-                    arr[wall3.x][wall3.y] = 1;
-                    
-                    virusBfs();
-                    maxCnt = Math.max(cntSafeZone(), maxCnt);
-                    visited = new boolean[N][M];
-                    
-                    arr[wall1.x][wall1.y] = 0;
-                    arr[wall2.x][wall2.y] = 0;
-                    arr[wall3.x][wall3.y] = 0;
-                } 
-            }
-        } 
-
-        System.out.print(maxCnt);
-        
+        visited = new boolean[walls.size()];
+        dfs(0, 0);
+        System.out.print(maxSafe);
     }
     
-    static void virusBfs() {
-        Queue<Position> q = new LinkedList<>();
-        tempArr = new int[N][M];
-
+    static void dfs(int start, int depth) {
+        if (depth == 3) {
+            bfs();
+            return;
+        }
+        
+        for (int i = start; i < walls.size(); i++) {
+            visited[i] = true;
+            dfs(i + 1, depth + 1);
+            visited[i] = false;
+        }
+    }
+    
+    static void bfs() {
+        Queue<int[]> viruses = new LinkedList<>();
+        int[][] copy = new int[N][M];
+        boolean[][] spread = new boolean[N][M];
+        
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                tempArr[i][j] = arr[i][j];
+                copy[i][j] = arr[i][j];
                 
-                if (tempArr[i][j] == 2) {
-                    q.add(new Position(i,j));
+                if (copy[i][j] == 2) {
+                    spread[i][j] = true;
+                    viruses.add(new int[]{i, j});
                 }
             }
         }
-
-        while(!q.isEmpty()) {
-            Position curPos = q.poll();
+        
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i]) {
+                int[] wall = walls.get(i);
+                copy[wall[0]][wall[1]] = 1;
+            }
+        }
+        
+        int[] dr = {-1,1,0,0};
+        int[] dc = {0,0,-1,1};
+        
+        while (!viruses.isEmpty()) {
+            int[] virus = viruses.poll();
             
             for (int i = 0; i < 4; i++) {
-                int moveX = curPos.x + dx[i];
-                int moveY = curPos.y + dy[i];
+                int nr = virus[0] + dr[i];
+                int nc = virus[1] + dc[i];
                 
-                if (moveX >= 0 && moveX < N && moveY >= 0 && moveY < M) {
-                    if (!visited[moveX][moveY] && tempArr[moveX][moveY] == 0) {
-                        tempArr[moveX][moveY] = 2;
-                        visited[moveX][moveY] = true;
-                        q.add(new Position(moveX, moveY));
-                    }
-                }
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if (spread[nr][nc]) continue;
+                if (copy[nr][nc] == 1) continue;
+                
+                viruses.add(new int[]{nr, nc});
+                spread[nr][nc] = true;
+                copy[nr][nc] = 2;
             }
         }
-    }
-    
-    static int cntSafeZone() {
-        int cnt = 0;
+        
+        int safe = 0;
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (tempArr[i][j] == 0) {
-                    cnt++;
+            for (int j = 0 ; j < M; j++) {
+                if (copy[i][j] == 0) {
+                    safe++;
                 }
             }
         }
         
-        return cnt;
+        maxSafe = Math.max(safe, maxSafe);
     }
 }
