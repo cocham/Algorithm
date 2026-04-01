17677d50830087b48c05af631090f787c7877516
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-01 11:12:57 +0900

    [Gold IV] Title: 인구 이동, Time: 668 ms, Memory: 298904 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -2,15 +2,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.ArrayList;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Main {
     static int N, L, R;
     static int[][] arr;
-    static int[] dr = {-1,1,0,0};
-    static int[] dc = {0,0,-1,1};
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,12 +25,10 @@ public class Main {
             }
         }
         
-        
         int day = 0;
-        
-        while(true) {
-            boolean[][] visited = new boolean[N][N];
+        while (true) {
             boolean isMoved = false;
+            boolean[][] visited = new boolean[N][N];
             
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -43,57 +39,58 @@ public class Main {
                     }
                 }
             }
-           
-            if (!isMoved) break;
             
+            if (!isMoved) break;
             day++;
         }
-        
+
         System.out.print(day);
     }
     
     static boolean bfs(int r, int c, boolean[][] visited) {
         Queue<int[]> q = new LinkedList<>();
-        ArrayList<int[]> group = new ArrayList<>();
+        ArrayList<int[]> union = new ArrayList<>();
+        int unionP = arr[r][c];
         
-        visited[r][c] = true;
+        int[] dr = {-1,1,0,0};
+        int[] dc = {0,0,-1,1};
+        
+        visited[r][c] = true;   
         q.add(new int[]{r, c});
-        group.add(new int[]{r, c});
+        union.add(new int[]{r, c});
         
-        int sum = arr[r][c];
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             
-            for (int d = 0; d < 4; d++) {
-                int nr = cur[0] + dr[d];
-                int nc = cur[1] + dc[d];
-                            
+            for (int i = 0; i < 4; i++) {
+                int nr = cur[0] + dr[i];
+                int nc = cur[1] + dc[i];
+                
                 if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
                 if (visited[nr][nc]) continue;
                 
                 int diff = Math.abs(arr[cur[0]][cur[1]] - arr[nr][nc]);
-                if (diff >= L && diff <= R) {
+            
+                if (diff >= L && diff <= R){
                     q.add(new int[]{nr, nc});
-                    group.add(new int[]{nr, nc});
+                    union.add(new int[]{nr, nc});
                     visited[nr][nc] = true;
-                    sum += arr[nr][nc];
-                }
+                    unionP += arr[nr][nc];
+                }    
             }
         }
         
-        if (group.size() > 1) {
-            int people = sum / group.size();
-            for (int i = 0; i < group.size(); i++) {
-                int[] country = group.get(i);
-                int curR = country[0];
-                int curC = country[1];
+        if (union.size() > 1) {
+            int divideP = unionP / union.size();
             
-                arr[curR][curC] = people;
+            for (int[] country : union) {
+                int R = country[0];
+                int C = country[1];
+                arr[R][C] = divideP;
             }
             
             return true;
         }
-
-        return false;
+       return false;
     }
 }
