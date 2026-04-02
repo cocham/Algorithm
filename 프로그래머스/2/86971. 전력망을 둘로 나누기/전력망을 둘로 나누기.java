43865d701dba4a718802cafe979f21ab859c9e6d
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-02 10:47:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.39 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,59 +1,64 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-    static int n;
-    static int minDiff;
+    
     static ArrayList<Integer>[] tree;
+    static int minDiff = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
         tree = new ArrayList[n + 1];
-        this.n = n;
-        minDiff = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < tree.length; i++) {
+        for (int i = 1; i <= n; i++) {
             tree[i] = new ArrayList<>();
         }
         
         for (int i = 0; i < wires.length; i++) {
-            int root = wires[i][0];
-            int child = wires[i][1];
+            int s = wires[i][0];
+            int to = wires[i][1];
             
-            tree[root].add(child);
-            tree[child].add(root);
+            tree[s].add(to);
+            tree[to].add(s);
         }
         
         for (int[] wire : wires) {
             int a = wire[0];
             int b = wire[1];
             
-            tree[a].remove(Integer.valueOf(b));
-            tree[b].remove(Integer.valueOf(a));
+            ArrayList<Integer> agroup = tree[a];
+            ArrayList<Integer> bgroup = tree[b];
             
-            boolean[] visited = new boolean[n + 1];
+            agroup.remove(Integer.valueOf(b));
+            bgroup.remove(Integer.valueOf(a));
             
-            int aGroup = dfs(a, visited);
-            int bGroup = n - aGroup;
+            int acnt = bfs(a,n);
+            int bcnt = bfs(b,n);
             
-            tree[a].add(b);
-            tree[b].add(a);
-            
-            int diff = Math.abs(aGroup - bGroup);
-            minDiff = Math.min(diff, minDiff);
+            agroup.add(b);
+            bgroup.add(a);
+            minDiff = Math.min(Math.abs(acnt - bcnt), minDiff);
         }
         
         return minDiff;
     }
     
-    static int dfs(int node, boolean[] visited) {
-        visited[node] = true;
-        int cnt = 1;
+    static int bfs(int start, int n) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n + 1];
+        q.add(start);
+        visited[start] = true;
         
-        for (int n : tree[node]) {
-            if (!visited[n]) {
-                cnt += dfs(n, visited);
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            
+            for (int num : tree[cur]) {
+                if (visited[num]) continue;
+                visited[num] = true;
+                q.add(num);
+                cnt++;
             }
         }
         
         return cnt;
     }
+    
 }
