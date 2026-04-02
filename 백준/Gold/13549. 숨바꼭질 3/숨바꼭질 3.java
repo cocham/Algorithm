316ba229bdb153e561ca501380a2476c7e875dfe
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-02 13:31:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 20128 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -2,55 +2,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
-    static int limit = 100000;
-    static int N;
-    static int K;
-    static int[] time = new int[100001];
+    static int N, K;
+    static int[] arr;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        Arrays.fill(time, -1);
-        bfs(N, K);
-        System.out.print(time[K]);
+        
+        arr = new int[100001];
+        System.out.print(bfs());
     }
     
-    static void bfs(int start, int target) {
-        Deque<Integer> q = new ArrayDeque<>();
-        time[start] = 0;
-        q.add(start);
+    static int bfs() {
+        int[] minTime = new int[100001];
+        for (int i = 0; i < minTime.length; i++) {
+            minTime[i] = Integer.MAX_VALUE;
+        }
+        Queue<int[]> q = new LinkedList<>();
         
-        while(!q.isEmpty()) {
-            int num = q.poll();
+        q.add(new int[]{N, 0});
+        minTime[N] = 0;
+        int[] direc = {-1, 1, 2};
+        
+        while (!q.isEmpty()) {
+            int[] x = q.poll();
+            int r = x[0];
+            int curT = x[1];
+                
+            if (r == K) break;
             
-            if (num == target) {
-                return;
-            }
-            
-            int step3 = num * 2;
-            int step1 = num - 1;
-            int step2 = num + 1;
-            
-            if (step3 >= 0 && step3 <= limit && time[step3] == -1) {
-                time[step3] = time[num];
-                q.addFirst(step3);
-            }
-            if (step1 >= 0 && step1 <= limit && time[step1] == -1) {
-                time[step1] = time[num] + 1;
-                q.addLast(step1);
-            } 
-            if (step2 >= 0 && step2 <= limit && time[step2] == -1) {
-                time[step2] = time[num] + 1;
-                q.addLast(step2);
+            for (int i = 0; i < 3; i++) {
+                int nr = r + direc[i];
+                int t = 1;
+                if (i == 2) {
+                    nr = r * 2;
+                    t = 0;
+                }
+
+                if (nr < 0 || nr > 100000) continue;
+                if (curT + t < minTime[nr]) {
+                    minTime[nr] = curT + t;
+                    q.add(new int[]{nr, curT + t});
+                }
             }
         }
+        
+        return minTime[K];
+        
     }
 }
