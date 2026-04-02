6a829a392950beec3fe60cc905b716e7c66b5012
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-02 11:20:05 +0900

    [level 2] Title: 비밀 코드 해독, Time: 13.99 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,48 +1,44 @@
 class Solution {
-    static int[] selected;
-    static int cnt;
+    static boolean[] selected;
+    static int result;
     
     public int solution(int n, int[][] q, int[] ans) {
-        selected = new int[5];
-        dfs(1, 0, n, q, ans);
-        return cnt;
+        result = 0;
+        selected = new boolean[n + 1];
+        dfs(q, ans,  1, n, 0);
+        return result;
     }
     
-    static void dfs(int pick, int depth, int n, int[][] q, int[] ans) {
-        if (depth == 5) {
-            if (isValid(selected, q, ans)) {
-                cnt++;
-            }
-            
+    static void dfs(int[][] q, int[] ans, int start, int n, int cnt) {
+        if (cnt == 5) {
+            check(q, ans, n);
             return;
         }
         
-        for (int i = pick; i <= n; i++) {
-            selected[depth] = i;
-
-            dfs(i + 1, depth + 1, n, q, ans);
+        for (int i = start; i <= n; i++) {
+            selected[i] = true;
+            dfs(q, ans, i + 1, n, cnt + 1);
+            selected[i] = false;
         }
     }
     
-    static boolean isValid(int[] pick, int[][] q, int[] ans) {
+    static void check(int[][] q, int[] ans, int n) {
         
         for (int i = 0; i < q.length; i++) {
-            int sameCnt = ans[i];
-            int cnt = 0;
+            int correct = ans[i];
+            int curCorrec = 0;
             
             for (int j = 0; j < q[i].length; j++) {
-                for (int k = 0; k < pick.length; k++) {
-                    if (q[i][j] == pick[k]) {
-                        cnt++;
-                    }
+                if (selected[q[i][j]]) {
+                    curCorrec++;
                 }
             }
             
-            if (cnt != sameCnt) {
-                return false;
+            if (curCorrec != correct) {
+                return;
             }
         }
         
-        return true;
+        result++;
     }
 }
