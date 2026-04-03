5f3ad504b6b3c4261e4bf06188d1dfe12043b7a3
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-03 12:15:35 +0900

    [level 3] Title: 에어컨, Time: 6.09 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/에어컨.java
+++ 프로그래머스/3/214289. 에어컨/에어컨.java
@@ -1,55 +1,62 @@
 class Solution {
     public int solution(int temperature, int t1, int t2, int a, int b, int[] onboard) {
-        
-        int INF = 1000 * 100; 
-        int[][] dp = new int[onboard.length][51];
-        for (int i = 0; i < onboard.length; i++) {
-            for (int t = 0; t <= 50; t++) {
-                dp[i][t] = INF;
+        int INF = 100000;
+        int time = onboard.length;
+        int[][] dp = new int[time][51]; //값 = 최소 소비 전력량
+        for (int i = 0; i < time; i++) {
+            for (int j = 0; j <= 50; j++) {
+                dp[i][j] = INF;
             }
         }
         
-        int startTemp = temperature + 10;
-        dp[0][startTemp] = 0; //0분에 실외온도 == 실내온도 상태면 소비전력은 0
+        int outTemp = temperature + 10;
+        dp[0][outTemp] = 0;
         
-        for (int i = 1; i < onboard.length; i++) {
+        t1 += 10;
+        t2 += 10;
+        
+        for (int i = 1; i < time; i++) {
             for (int t = 0; t <= 50; t++) {
-                if (onboard[i] == 1 && (t < t1 + 10 || t > t2 + 10)) continue;
+                if (onboard[i] == 1 && (t < t1 || t > t2)) continue; //승객이 탑승 중에는 항상 쾌적해야됨
                 
-                int minCost = INF;
-                // 이전 온도: t - 1
-                if (t > 0) {
-                    minCost = Math.min(dp[i - 1][t - 1] + a, minCost);
-                    
-                    if (t - 1 < startTemp) {
-                        minCost = Math.min(dp[i - 1][t - 1], minCost);
+                int minPower = INF;
+                
+                // 온도 상승
+                if (t - 1 >= 0) {
+                    // 에어컨 OFF
+                    if (t - 1 < outTemp) {
+                        minPower = Math.min(dp[i - 1][t - 1], minPower);
                     }
-                }
-                
-                // 이전 온도 : t + 1
-                if (t < 50) {
-                    minCost = Math.min(dp[i-1][t+1] + a, minCost);
-                    if (t + 1 > startTemp) {
-                        minCost = Math.min(dp[i-1][t+1], minCost);
-                    }
-                }
-                
-                // 유지
-                minCost = Math.min(minCost, dp[i-1][t] + b);
-                
-                if (t == startTemp) {
-                    minCost = Math.min(minCost, dp[i-1][t]);
-                }
                     
-                dp[i][t] = minCost;
+                    // 에어컨 ON
+                    minPower = Math.min(dp[i - 1][t - 1] + a, minPower);
+                }
+                
+                // 온도 하강
+                if (t + 1 <= 50) {
+                    // 에어컨 OFF
+                    if (t + 1 > outTemp) {
+                        minPower = Math.min(dp[i - 1][t + 1], minPower);
+                    }
+                    // 에어컨 ON
+                    minPower = Math.min(dp[i - 1][t + 1] + a, minPower);
+                }
+                
+                // 온도 유지
+                // 에어컨 OFF
+                if (t == outTemp) {
+                    minPower = Math.min(dp[i-1][t], minPower);
+                }
+                
+                minPower = Math.min(dp[i-1][t] + b, minPower);
+                
+                dp[i][t] = minPower;
             }
         }
         
         int answer = INF;
-        
         for (int t = 0; t <= 50; t++) {
-            answer = Math.min(dp[onboard.length - 1][t], answer);
-            
+            answer = Math.min(dp[time - 1][t], answer);
         }
         
         return answer;
