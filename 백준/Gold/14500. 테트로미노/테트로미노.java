06ebaf81eebc23b0610f16cf541349c0827d7c9e
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-07 11:04:12 +0900

    [Gold IV] Title: 테트로미노, Time: 648 ms, Memory: 121944 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -4,19 +4,20 @@ import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] arr;
-    static int[] dr = {-1,1,0,0};
+    static int[] dr = {1,-1,0,0};
     static int[] dc = {0,0,-1,1};
-    static int ans = 0;
+    static int N,M;
+    static int[][] arr;
+    static int max = 0;
+    static boolean[][] visited;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
+        visited = new boolean[N][M];
         
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,52 +28,65 @@ public class Main {
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                dfs(i, j, -1, -1, arr[i][j], 1);
+                visited[i][j] = true;
+                dfs(i, j, 1, arr[i][j]);
+                visited[i][j] = false;
                 
-                checkT(i, j);
+                checkT(i,j);
             }
         }
-              
-        System.out.println(ans);
+
+        System.out.print(max);
+                
     }
     
-    static void dfs(int r, int c, int prevR, int prevC, int sum, int depth) {
-        if (depth == 4) {
-            ans = Math.max(sum, ans);
+    static void dfs(int r, int c, int slot, int sum) {
+        if (slot == 4) {
+            max = Math.max(sum, max);
             return;
         }
         
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
             
             if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-            if (nr == prevR && nc == prevC) continue;
+            if (visited[nr][nc]) continue;
             
-            dfs(nr, nc, r, c, sum + arr[nr][nc], depth + 1);
-        }        
+            visited[nr][nc] = true;
+            dfs(nr, nc, slot + 1, sum + arr[nr][nc]);
+            visited[nr][nc] = false;
+        }
     }
     
     static void checkT(int r, int c) {
-        // ㅜ
-        if (r + 1 < N && c + 1 < M && c - 1 >= 0) {
-            int sum = arr[r][c] + arr[r + 1][c] + arr[r][c + 1] + arr[r][c - 1];
-            ans = Math.max(sum, ans);
-        } 
-        // ㅗ
-        if (r - 1 >= 0 && c + 1 < M && c - 1 >= 0) {
-            int sum = arr[r][c] + arr[r - 1][c] + arr[r][c + 1] + arr[r][c - 1];
-            ans = Math.max(sum, ans);
-        }
-        // ㅓ
-        if (c - 1 >= 0 && r - 1 >= 0 && r + 1 < N) {
-            int sum = arr[r][c] + arr[r][c - 1] + arr[r - 1][c] + arr[r + 1][c];
-            ans = Math.max(sum, ans);
-        }
-        // ㅏ
-        if (c + 1 < M && r - 1 >= 0 && r + 1 < N) {
-            int sum = arr[r][c] + arr[r][c + 1] + arr[r - 1][c] + arr[r + 1][c];
-            ans = Math.max(sum, ans);
+        int[][][] shapes = {
+            {{0,1}, {0,2}, {1,1}}, //ㅜ
+            {{0,1}, {0,2}, {-1,1}}, //ㅗ        
+            {{1,0}, {2,0}, {1,1}}, //ㅏ               
+            {{0,1}, {-1,1}, {1,1}} //ㅓ
+        } ;
+    
+        
+        for (int[][] shape : shapes) {
+            boolean valid = true;
+            int sum = arr[r][c];
+
+            for (int[] d : shape) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+            
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
+                    valid = false;
+                    break;
+                }
+            
+                sum += arr[nr][nc];
+            }
+            
+            if (valid) {
+                max = Math.max(sum, max);
+            }
         }
     }
 }
