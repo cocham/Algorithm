ac0ef3de4e47675bc37ca3571b0f34367163e172
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-10 15:09:30 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,20 +1,20 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int arr[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         long sum = 0;
-        for (int n : arr){
-            sum += n * n;
+        for (int i = 0; i < 5; i++) {
+            long n = Integer.parseInt(st.nextToken());
+            sum += (n * n);
         }
-        bw.write(Long.toString(sum % 10));
-        bw.flush();
-        bw.close();
-        br.close();
+        
+        System.out.print(sum % 10);
+
     }
 }
