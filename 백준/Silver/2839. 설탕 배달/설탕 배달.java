90199bba397ab3bab501cbc52fa5cc3985b302b6
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-15 10:59:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int cnt = 0;
+        while (N >= 0) {
+            if (N % 5 == 0) {
+                cnt += N / 5;
+                System.out.print(cnt);
+                return;
+            }
+            
+            N -= 3;
+            cnt++;
+        }
+        
+        System.out.print(-1);
+
+    }
+}
