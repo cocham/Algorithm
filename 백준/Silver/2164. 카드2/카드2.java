4b6758d6a5bd883058833d8ddfa7dc1ccbe6dff5
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-04-15 10:41:46 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28944 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,26 +1,25 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         
-        int n = Integer.parseInt(br.readLine());
-        Deque<Integer> queue = new ArrayDeque<>();
-        
-        for (int i = 1; i <= n; i++) {
-            queue.addLast(i);
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            dq.addLast(i);
         }
         
-        while(queue.size() > 1) {
-            queue.poll();
-            queue.addLast(queue.poll());
+        while (dq.size() > 1) {
+            dq.poll();
+            dq.addLast(dq.poll());
         }
         
-        System.out.println(queue.poll());
-        
+        System.out.print(dq.poll());
     }
 }
