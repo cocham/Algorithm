1f8a84953feece3c1d46e00269301cce73f30420
Kim min hui <91892334+cocham@users.noreply.github.com>
2026-05-17 12:29:52 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 7.30 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,152 +1,147 @@
 import java.util.*;
 
 class Solution {
-    static int N;
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
+    static ArrayList<int[]> emptyBoard = new ArrayList<>();
     
-    static class Position {
-        int r, c;
-        
-        Position (int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
     
-    public int solution(int[][] game_board, int[][] table) {
-        N = game_board.length;
+    public int solution(int[][] game_board, int[][] table) {        
+        int n = game_board.length;
         
-        List<List<Position>> emptySpaces = new ArrayList<>();
-        List<List<Position>> existSpaces = new ArrayList<>();
-        boolean[][] gVisited = new boolean[N][N];
-        boolean[][] tVisited = new boolean[N][N];
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (game_board[i][j] == 0 && !gVisited[i][j]) {
-                    emptySpaces.add(bfs(game_board, gVisited,i,j,0));
-                }
-                if (table[i][j] == 1 && !tVisited[i][j]) {
-                    existSpaces.add(bfs(table, tVisited,i,j,1));
-                }
-            }
-        }
-        
-        return compare(emptySpaces, existSpaces);
-    }
-    
-    static int compare(List<List<Position>> game, List<List<Position>> puzzles) {
-        boolean[] usedPuzzle = new boolean[puzzles.size()];
-        int totalFilled = 0;
-        
-        for (List<Position> empty : game) {
-            
-            for (int i = 0; i < puzzles.size(); i++) {
-                if(usedPuzzle[i]) {
-                    continue;
+        List<List<int[]>> emptySpaces = new ArrayList<>();
+        List<List<int[]>> pieces = new ArrayList<>();
+        boolean[][] tvisited = new boolean[n][n];
+        boolean[][] visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (game_board[i][j] == 0 && !visited[i][j]) {
+                    emptySpaces.add(bfs(game_board, i , j, n, 0, visited));
                 }
                 
-                List<Position> puzzle = puzzles.get(i);
-                if (empty.size() == puzzle.size()) {
-                    if (isRotationMatch(empty, puzzle)) {
-                        usedPuzzle[i] = true;
-                        totalFilled += puzzle.size();
-                        break;
-                    }
+                if (table[i][j] == 1 && !tvisited[i][j]) {
+                    pieces.add(bfs(table, i, j, n, 1, tvisited));
                 }
             }
         }
         
-        return totalFilled;
+        for (List<int[]> em : emptySpaces) {
+            normalize(em);
+        }
+        
+        for (List<int[]> p : pieces) {
+            normalize(p);
+        }
+        
+        int answer = compare(emptySpaces, pieces);
+        return answer;
     }
     
-    static boolean isRotationMatch(List<Position> empty, List<Position> puzzle) {
-        List<Position> target = normalize(empty);
-
-        for (int i = 0; i < 4; i++) {
-            puzzle = normalize(rotate(puzzle));
-            
-            if (isSame(target, puzzle)) {
-                return true;
+    //매칭
+    static int compare(List<List<int[]>> emptySpaces, List<List<int[]>> pieces) {
+        boolean[] used = new boolean[pieces.size()];
+        int fill = 0;
+        
+        for (List<int[]> empty : emptySpaces) {
+            for (int i = 0; i < pieces.size(); i++) {
+                if (used[i]) continue;
+                
+                List<int[]> piece = pieces.get(i);
+                if (piece.size() != empty.size()) continue;
+                
+                boolean isMatched = false;
+                for (int d = 0; d < 4; d++) {
+                    if (isMatch(empty, piece)) {
+                        isMatched = true;
+                        break;
+                    }
+                    rotate(piece);
+                }
+                
+                if (isMatched) {
+                    used[i] = true;
+                    fill += piece.size();
+                    break;
+                }
             }
         }
         
-        return false;
+        return fill;
     }
     
-    static boolean isSame(List<Position> a, List<Position> b) {
-        for (int j = 0; j < a.size(); j++) {
-            Position pA = a.get(j);
-            Position pB = b.get(j);
-
-            if (pA.r != pB.r || pA.c != pB.c) {
+    static boolean isMatch(List<int[]> empty, List<int[]> pieces) {
+        for (int i = 0; i < empty.size(); i++) {
+            if (empty.get(i)[0] != pieces.get(i)[0] || empty.get(i)[1] != pieces.get(i)[1]) {
                 return false;
             }
         }
         
         return true;
     }
-    static List<Position> rotate(List<Position> shape) {
-        List<Position> rotated = new ArrayList<>();
-        
-        for (Position p : shape) {
-            rotated.add(new Position(p.c, -p.r));
-        }
-        
-        return rotated;
-    }
-
-    static List<Position> normalize(List<Position> shape) {
+    
+    //좌표 정규화    
+    static List<int[]> normalize(List<int[]> block) {
         int minR = Integer.MAX_VALUE;
         int minC = Integer.MAX_VALUE;
-    
-        for (Position p : shape) {
-            minR = Math.min(p.r, minR);
-            minC = Math.min(p.c, minC);
+        
+        for (int[] p : block) {
+            minR = Math.min(p[0], minR);
+            minC = Math.min(p[1], minC);
         }
         
-        List<Position> normalized = new ArrayList<>();
-        for (Position p : shape) {
-            normalized.add(new Position(p.r - minR, p.c - minC));
+        for (int[] p : block) {
+            p[0] -= minR;
+            p[1] -= minC;
         }
         
-        Collections.sort(normalized, (p1, p2) -> {
-            if (p1.r == p2.r) return p1.c - p2.c;
-            return p1.r - p2.r;
+        Collections.sort(block, (a, b) -> {
+            if (a[0] == b[0]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
         });
-            
-        return normalized;
+        
+        return block;
     }
     
+    // 회전
+    static void rotate(List<int[]> block) {
+        for (int[] p : block) {
+            int temp = p[0];
+            p[0] = p[1];
+            p[1] = -temp;            
+        }
+        
+        normalize(block);
+    }
     
-    static List<Position> bfs(int[][] arr, boolean[][] visited, int row, int col, int type) {
-        Queue<Position> q = new LinkedList<>();
-        List<Position> part = new ArrayList<>();
-        Position start = new Position(row, col);
+    // 덩어리 분리
+    static List<int[]> bfs(int[][] board, int i, int j, int n, int target, boolean[][] visited) {
+        List<int[]> group = new ArrayList<>();
+        Queue<int[]> q = new LinkedList<>();
+        visited[i][j] = true;
+        q.add(new int[]{i, j});
+        group.add(new int[]{i, j});
         
-        part.add(start);
-        q.add(start);
-        visited[row][col] = true;
-        
-        while(!q.isEmpty()) {
-            Position cur = q.poll();
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
             
-            for (int i = 0; i < 4; i++) {
-                int nr = cur.r + dr[i];
-                int nc = cur.c + dc[i];
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
                 
-                if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
-                    if (!visited[nr][nc] && arr[nr][nc] == type) {
-                        q.add(new Position(nr,nc));
-                        part.add(new Position(nr,nc));
-                        visited[nr][nc] = true;
-                    }
-                }
+                if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
+                if (visited[nr][nc]) continue;
+                if (board[nr][nc] != target) continue;
+                
+                group.add(new int[]{nr, nc});
+                visited[nr][nc] = true;  
+                q.add(new int[]{nr, nc});
             }
         }
         
-        return part;
-        
+        return group;
     }
+    
 }
