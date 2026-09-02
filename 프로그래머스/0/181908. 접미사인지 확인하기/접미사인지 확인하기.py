7bd74a007a0ee2472060259fc3a07109671d97b3
Kim min hee <91892334+cocham@users.noreply.github.com>
2026-09-02 21:53:39 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,6 @@
+def solution(my_string, is_suffix):
+    for i in range(len(my_string)):
+        if (my_string[i] == is_suffix[0]):
+            if (my_string[i:] == is_suffix):
+                return 1
+    return 0
